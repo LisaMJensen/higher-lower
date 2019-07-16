be15395b7f8df3b11e82e438a261609506769367
Marqis Ramos and Na Hyung Choi <marqis@>
2019-07-16 13:43:05 -0700

    prompt user for a guess and make first guess

--- Project.cs
+++ Project.cs
@@ -1,9 +1,28 @@
 using System;
 
-class ProjectName
+class Highlow
+{
+  public static int index;
+
+  public static void Randomize()
+  {
+    Random rand = new Random();
+    index = rand.Next(1, 101);
+  }
+}
+
+class Program
 {
   static void Main()
   {
-    
+    Console.WriteLine("Would you like to play? Y or N");
+    string answer = Console.ReadLine();
+
+    if (answer == "y" || answer == "Y")
+    {
+      Console.WriteLine("Choose a number between 1 and 100!");
+      Highlow.Randomize();
+      Console.WriteLine("Is your number higher or lower than " + Highlow.index + "? (Higher/Lower/Correct)");
+    }
   }
 }

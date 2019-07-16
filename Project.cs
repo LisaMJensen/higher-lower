fda34ccc63fc3e4f0f3e9c23e402b33ea7ce28d9
Marqis Ramos and Na Hyung Choi <marqis+na@>
2019-07-16 14:26:50 -0700

    computer guesses your number in a smart way

--- Project.cs
+++ Project.cs
@@ -3,23 +3,33 @@ using System;
 class Highlow
 {
   public static int number;
+  public static int highestLowerLimit = 1;
+  public static int lowestHigherLimit = 101;
 
   public static void initialGuess()
   {
     Random rand = new Random();
-    number = rand.Next(1, 101);
+    number = rand.Next(highestLowerLimit, lowestHigherLimit);
   }
 
   public static void higherGuess(int lowerLimit)
   {
     Random rand = new Random();
-    number = rand.Next(lowerLimit, 101);
+    if (lowerLimit > highestLowerLimit)
+    {
+      highestLowerLimit = lowerLimit;
+    }
+    number = rand.Next(highestLowerLimit, lowestHigherLimit);
   }
 
   public static void lowerGuess(int higherLimit)
   {
     Random rand = new Random();
-    number = rand.Next(1, higherLimit);
+    if (higherLimit < lowestHigherLimit)
+    {
+      lowestHigherLimit = higherLimit;
+    }
+    number = rand.Next(highestLowerLimit, lowestHigherLimit);
   }
 }
 
@@ -32,34 +42,34 @@ class Program
     Console.WriteLine("Is your number higher or lower than " + Highlow.number + "? (Higher/Lower/Correct)");
     answer = Console.ReadLine();
 
-    if (answer == "correct" || answer == "Correct")
+    if (answer == "correct" || answer == "Correct" || answer == "c")
     {
       Console.WriteLine("Great! Thanks for playing!");
     }
-    else if (answer == "Higher" || answer == "higher")
+    else if (answer == "Higher" || answer == "higher" || answer == "h")
     {
       Highlow.higherGuess(Highlow.number);
       makeGuesses();
     }
-    else if (answer == "Lower" || answer == "lower")
+    else if (answer == "Lower" || answer == "lower" || answer == "l")
     {
       Highlow.lowerGuess(Highlow.number);
       makeGuesses();
     }
     else
     {
-      Console.WriteLine("Please enter a valid response. (Higher/Lower/Correct)");
+      Console.WriteLine("Please enter a valid response.");
+      makeGuesses();
     }
   }
 
   static void Main()
   {
-    Console.WriteLine("Would you like to play? Y or N");
+    Console.WriteLine("Would you like to play? Hit Y when you have chosen a number between 1 and 100");
     answer = Console.ReadLine();
 
     if (answer == "y" || answer == "Y")
     {
-      Console.WriteLine("Choose a number between 1 and 100!");
       Highlow.initialGuess();
       makeGuesses();
     }
